9f1ac6e595daed8b1846f456db370f146736dba3
egor7531 <egorkokonin557@gmail.com>
2023-10-21 20:35:21 +0300

    add  treatment of file ASM

--- Assembler.cpp
+++ Assembler.cpp
@@ -1,12 +1,73 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
-void WriteByteCode(FILE * fpin)
+int GetFileSize(FILE * fp);
+char * GetFileContent(FILE * fp, const int fileSize);
+void WriteByteCode(const char * buffer);
+
+int main()
 {
-    assert(fpin != NULL);
+    const char * nameFileIn  = "ASM.txt";
 
-    const char * nameFileOut = "ByteCode.txt";
+    FILE * fpin = fopen(nameFileIn, "rb"); //в каком режиме открывать? проблема в символе \r
+
+    if(fpin == NULL)
+    {
+        printf("can't open file\n");
+        return -1;
+    }
+
+    int fpinSize = GetFileSize(fpin);
+
+    char * buffer = GetFileContent(fpin, fpinSize);
+
+    fclose(fpin);
+
+
+
+    WriteByteCode(buffer);
+
+    return 0;
+}
+
+int GetFileSize(FILE * fp)
+{
+    struct stat st;
+
+    fstat(fileno(fp), &st);
+
+    return st.st_size;
+}
+
+char * GetFileContent(FILE * fp, const int fileSize)
+{
+    assert(fp != NULL);
+    assert(fileSize > 0);
+
+    char * buf = (char *)cafferlloc(fileSize, sizeof(char));
+
+    if(buf == NULL)
+        printf("Pointer on buffer is NULL\n");
+
+    if(fread(buf, sizeof(char), fileSize, fp) != fileSize)
+    {
+        if(feof(fp))
+            printf("Premature end of file\n");
+
+        else
+            printf("File read error\n");
+    }
+
+    return buf;
+}
+void WriteByteCode(const char * buffer)
+{
+    assert(buffer != NULL);
+
+    const char * nameFileOut = "ByteCode.bin";
     const int maxSizeCommand = 10;
 
     char command[maxSizeCommand] = {};
@@ -65,23 +126,4 @@ void WriteByteCode(FILE * fpin)
     }
 
     fclose(fpout);
-}
-
-int main()
-{
-    const char * nameFileIn  = "ASM.txt";
-
-    FILE * fpin = fopen(nameFileIn, "rb");
-
-    if(fpin == NULL)
-    {
-        printf("can't open file\n");
-        return -1;
-    }
-
-    WriteByteCode(fpin);
-
-    fclose(fpin);
-
-    return 0;
 }
