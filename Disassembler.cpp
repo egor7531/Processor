a4049f0e9ddb43ee0ff56fe891a5f61db298fe4a
egor7531 <egorkokonin557@gmail.com>
2023-10-23 18:20:35 +0300

    add codegeneration in disassembler

--- Disassembler.cpp
+++ Disassembler.cpp
@@ -1,59 +1,126 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-const char * nameFileIn  = "ByteCode.txt";
-const char * nameFileOut = "ASMcopy.txt";
+const int CHECK_CODE = 0b1111;
+const int IMM = 1 << 4;
+const int REG = 1 << 5;
 
-void FillASM(FILE * fpout, FILE * fpin, const int command)
-{
-    assert(fpout   != NULL);
-    assert(fpin    != NULL);
+static_assert( !(IMM & REG),         "IMM and REG overlap");
+static_assert( !(IMM & CHECK_CODE ), "IMM and CHECK_CODE  overlap");
+static_assert( !(REG & CHECK_CODE ), "REG and CHECK_CODE  overlap");
 
-    switch(command)
-    {
-        case -1: fprintf(fpout, "HLT\n");
-            break;
-        case 0: fprintf(fpout, "in\n");
-            break;
-        case 1: fprintf(fpout, "push ");
-            double value;    // double value = 0; - don't work ???
-            fscanf(fpin, "%lf", &value);
-            fprintf(fpout, "%.2lf\n", value);
-            break;
-        case 2: fprintf(fpout, "div\n");
-            break;
-        case 3: fprintf(fpout, "sub\n");
-            break;
-        case 4: fprintf(fpout, "out\n");
-            break;
-        case 5: fprintf(fpout, "add\n");
-            break;
-        case 6: fprintf(fpout, "mul\n");
-            break;
-        default: fprintf(fpout, "Unknown command\n");
-            break;
-    }
-}
+const char *regs_name[] = { "rax",
+                            "rbx",
+                            "rcx",
+                            "rdx",
+                            "rsi",
+                            "rdi",
+                            "rbp",
+                            "rsp",
+                            "r08",
+                            "r09",
+                            "r10",
+                            "r11",
+                            "r12",
+                            "r13",
+                            "r14",
+                            "r15"};
+
+const int REGISTERS_COUNT = sizeof(regs_name) / sizeof(regs_name[0]);
+
+int GetFileSize(FILE * fp);
+int * GetFileContent(int * sizeInstruct);
+void FillASM(int * instruct, int sizeInstruct);
 
 int main()
 {
-    FILE * fpin = fopen(nameFileIn, "rb");
+    int sizeInstruct = 0;
 
-    if(fpin == NULL)
-    {
-        printf("can't open file\n");
-        return -1;
-    }
+    int * instruct = GetFileContent(&sizeInstruct);
 
-    int command = 0;
-
-    FILE * fpout = fopen(nameFileOut, "wb");
-
-    while(fscanf(fpin, "%d", &command) != EOF)
-        FillASM(fpout, fpin, command);
-
-    fclose(fpin);
-    fclose(fpout);
+    FillASM(instruct, sizeInstruct);
 
     return 0;
+}
+
+int GetFileSize(FILE * fp)
+{
+    struct stat st;
+
+    fstat(fileno(fp), &st);
+
+    return st.st_size;
+}
+
+int * GetFileContent(int * sizeInstruct)
+{
+    const char * nameFile = "ByteCode.bin";
+    FILE *fp = fopen(nameFile, "rb");
+
+    if(fp == NULL)
+        printf("Can't open file\n");
+
+    *sizeInstruct = GetFileSize(fp) / sizeof(int);
+
+    int * instruct = (int *)calloc(GetFileSize(fp), sizeof(char));
+
+    if(instruct == NULL)
+        printf("Pointer on instructions is NULL\n");
+
+    if(fread(instruct, sizeof(int), *sizeInstruct, fp) != *sizeInstruct)
+    {
+        if(feof(fp))
+            printf("Premature end of file\n");
+
+        else
+            printf("File read error\n");
+    }
+
+    //for(int i = 0; i < countObject; i++)
+    //    printf("%d\n", instruct[i]);
+    fclose(fp);
+
+    return instruct;
+}
+
+void FillASM(int * instruct, int sizeInstruct)
+{
+    assert(instruct != NULL);
+    assert(sizeInstruct > 0);
+
+    const char * nameFile = "ASMcopy.txt";
+    FILE * fp = fopen(nameFile, "wb");
+
+    if(fp == NULL)
+        printf("Can't open file: %s\n", nameFile);
+
+    for(int i = 0; i < sizeInstruct; i++)
+    {
+        switch(instruct[i] & CHECK_CODE)
+        {
+            #define DEF_CMD(name, num, args, code)                              \
+                case num:                                                       \
+                    fprintf(fp, #name);                                         \
+                                                                                \
+                    if(args)                                                    \
+                    {                                                           \
+                        if(instruct[i] & IMM)                                   \
+                            fprintf(fp, " %d", instruct[++i]);                  \
+                                                                                \
+                        else if(instruct[i] & REG)                              \
+                            fprintf(fp, " %s", regs_name[instruct[++i] - 1]);   \
+                    }                                                           \
+                                                                                \
+                    fprintf(fp, "\n");                                          \
+                    break;                                                      \
+
+            #include "Command.h"
+            #undef DEF_CMD
+        }
+    }
+
+    fclose(fp);
 }
